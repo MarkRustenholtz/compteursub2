940ddfecc0e1820027d6604ea0faaf077b2bfcad
MarkRustenholtz <markrusten@gmail.com>
2025-09-27 16:44:44 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cr-gendarmerie-qrcode-v2'; // ⚠️ change le numéro à chaque mise à jour
+const CACHE_NAME = 'cr-gendarmerie-qrcode-v3'; // ⚠️ change le numéro à chaque mise à jour
 const urlsToCache = [
   './',
   './index.html',
